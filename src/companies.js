e81332a72e03bf19db612e78f7ce9d5fcd7f1ff9
Edwin Jude <edwin.judefrancis@gmail.com>
2024-12-01 20:08:28 +0530

    rearranging the directory for code readabilty

---
+++ src/companies.js
@@ -0,0 +1,8 @@
+import { faker } from "@faker-js/faker";
+
+export const companies = Array.from({ length: 15 }, () => {
+    return {
+      companyName: faker.company.name(),
+      phrase: faker.company.catchPhrase(),
+    };
+  });
---
+++ src/products.js
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+
+export const products = Array.from({ length: 20 }, () => {
+    return {
+      productName: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price(),
+    };
+  });
+  
